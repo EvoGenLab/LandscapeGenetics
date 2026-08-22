9dc9e038e70cbbcdaa5f02398391f10d2ce754cd
Heather Sibley <133798505+ResidentialDragonSlayer@users.noreply.github.com>
2026-08-22 18:18:36 -0400

    Revise comments and clarify dataset usage
    Updated comments for clarity and added dataset information.

--- gadma_sfs.sh
+++ gadma_sfs.sh
@@ -1,13 +1,11 @@
 # run from within conda env easySFS
+# Downloaded from: https://github.com/isaacovercast/easySFS
 
-./easySFS.py -i yvb_stairwayplot.vcf.gz -p pop_file.txt --preview -a > projvals.txt
-# projvals.txt was imported and maximum was found in R script `filtering_2024-2-23.R`
-# Values that maximize # segregating sites:
-# Samples=188, values=21012
-./easySFS.py -i yvb_stairwayplot.vcf.gz -p pop_file.txt -a --proj 188 --prefix yvb_run1
+# I believe yvb_gadma is just the base dataset with individuals from SIM & TL pops removed, then filtered for MAC=1
+# The popfile said all individuals were in the same population
 
-# for yvb_gone.recode.vcf, which we shall use for gadma
+
 ./easySFS.py -i yvb_gadma.recode.vcf -p popfile_yvb_gadma.txt --preview -a > projvals.txt
-# Assessed in R. Preview says:
+# Imported projvals.txt into R and found values that maximize # segregating sites
 # Samples=146, segsites=20451
 ./easySFS.py -i yvb_gadma.recode.vcf -p popfile_yvb_gadma.txt -a --proj 146 --prefix yvb_gadma -f
